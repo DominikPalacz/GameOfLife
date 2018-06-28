58f0ac2f59a6fe9f5ec388ec0110f458fe7e3507
palacz.dominik@gmail.com <palacz.dominik@gmail.com>
2018-06-28 12:01:05 +0200

    Building a board

--- js/app.js
+++ js/app.js
@@ -1,33 +1,33 @@
-document.addEventListener('DOMContentLoaded', (event) => {
-console.log('DOM fully loaded and parsed')
+document.addEventListener('DOMContentLoaded', () => {
+//console.log('DOM fully loaded and parsed');
 
-function GameOfLife(boardWidth, boardHeight) {
+    function GameOfLife(boardWidth, boardHeight) {
 
-    this.width = boardWidth;
-    this.height =boardHeight;
+        this.width = boardWidth;
+        this.height = boardHeight;
 
-}
+        this.board = document.getElementById('board');
+        //console.log(board);
 
-let game = new GameOfLife(10,10);
-console.log(game);
+        this.createBoard = function () {
+            this.board.style.width = (this.width * 10) + 'px';
+            this.board.style.height = (this.height * 10) + 'px';
 
+            let nrAllDiv = this.width * this.height;
+            //console.log(nrAllDiv);
 
+            for (let i = nrAllDiv; i > 0; i--) {
+                let div = document.createElement('div');
+                board.append(div);
+            }
 
+        }
 
+    }
 
+    let game = new GameOfLife(10, 10);
 
-
-
-
-
-
-
-
-
-
-
-
-
+    game.createBoard();
 
 
 });
