9958c11fa83fbeab0e3e95d75f080a1ceafa5c48
palacz.dominik@gmail.com <palacz.dominik@gmail.com>
2018-06-28 10:45:33 +0200

    Creating a game management object

--- js/app.js
+++ js/app.js
@@ -1,7 +1,15 @@
 document.addEventListener('DOMContentLoaded', (event) => {
 console.log('DOM fully loaded and parsed')
 
+function GameOfLife(boardWidth, boardHeight) {
 
+    this.width = boardWidth;
+    this.height =boardHeight;
+
+}
+
+let game = new GameOfLife(10,10);
+console.log(game);
 
 
 
